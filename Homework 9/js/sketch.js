33730eea3252ba9c2612b1fe577db180d6743728
AWilNol <134312897+AWilNol@users.noreply.github.com>
2023-10-24 03:08:48 -0600

    Start homework 9

---
+++ Homework 9/js/sketch.js
@@ -0,0 +1,12 @@
+function setup()
+  {
+    createCanvas(500,600);
+  }
+function draw()
+  {
+    background(120, 45, 145);
+    fill(126, 98, 245)
+    circle(250, 100, 65)
+    fill(200, 58, 590)
+    rect(250, 70, 100, 150)
+  }
