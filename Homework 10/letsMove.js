ad70da544fb69c3cd2a35e7de21b9bfca71086cb
AWilNol <134312897+AWilNol@users.noreply.github.com>
2023-11-02 10:17:17 -0600

    LetsMove file
    trying to separate the move file variables, but the console shows no changes. Did finally connect to the js file, yaya, love a good "save" when editing, as it makes things work better. Fun times over here.

--- Homework 10/letsMove.js
+++ Homework 10/letsMove.js
@@ -1,15 +1,25 @@
-var myFavoriteNumber = 42;
-     // this function is called only once
-    function setup()
-    {
-        createCanvas(800,600);
-    }
-    /* this function is called continuously
-        while the sketch is open in the browser
-    */
-    function draw()
-    {
-        background(120);
-        myFavoriteNumber++;
-        console.log(myFavoriteNumber);
-    }
+var myFavoriteNumber = 7;
+var name = "John Cena";
+var age = 67;
+var redColor = 123;
+var greenColor = 39;
+var blueColor = 21;
+
+var x = 100;
+var y = 200;
+var diameter = 50
+
+function doSomething()
+  {
+   var myFavoriteColor = "green";
+  }
+for(var i = 0; i < 5; i++)
+  {
+  console.log(i);
+  }
+function setup()
+  {
+    createCanvas(500,600);
+  } 
+function draw()
+  {
