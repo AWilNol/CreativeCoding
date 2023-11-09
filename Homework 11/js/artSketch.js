2223ac7987ec607c6657844c776eaac016263e74
AWilNol <134312897+AWilNol@users.noreply.github.com>
2023-11-09 01:49:37 -0700

    Update artSketch.js
    lessons

--- Homework 11/js/artSketch.js
+++ Homework 11/js/artSketch.js
@@ -10,35 +10,47 @@ function draw()
   background(40,98,215);
   fill(24,200,29);
   circle(x,y,diameter);
-    if (x > 300) {
-            x = 50;
-        }
-        else if (x > 200) {
-            x += 5;
-        }
-        else if (x <= 300) {
-            x += 10;
+  if(x <= 200)
+        {
+            x+=10;
         } 
+        else if(x > 200 && x <=300)
+        {
+            x+=8;
+            console.log("second else if for x");
+        }
+        else if(x > 300)
+        {
+           x = 50;
+        }
         
-        if (y > 300) {
+        if(y <= 200)
+        {
+            y+=12;
+        }
+          
+        else if(y > 200 && y <= 300)
+        {
+            y+=3; 
+            console.log("second else if for y");
+        }
+        else if(y > 300)
+        {
             y = 50;
         }
-        else if (y > 200) {
-            y += 1;
-        } 
-        else if (y <= 300) {
-            y += 3;
+        if(diameter <= 200)
+        {
+            diameter+=10;
         }
-
-        if (diameter > 300) {
+          
+        else if(diameter > 200 && diameter <= 300)
+        {
+            diameter +=4;
+            console.log("second else if for diameter");
+        }
+        else if(diameter > 300)
+        {
             diameter = 25;
         }
-        else if (diameter > 200) {
-            diameter += 4;
-            console.log("second else if for diameter");
-        } 
-        else if (diameter <= 300) {
-            diameter += 8;
-        } 
         
     }
