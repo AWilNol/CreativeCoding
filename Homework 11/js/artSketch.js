bc96fd6456406bfde71453b48b16a9486a364cbc
AWilNol <134312897+AWilNol@users.noreply.github.com>
2023-11-09 10:24:00 -0700

    Update artSketch.js
    i really need to get better at pushing these. exercises

--- Homework 11/js/artSketch.js
+++ Homework 11/js/artSketch.js
@@ -10,45 +10,46 @@ function draw()
   background(40,98,215);
   fill(24,200,29);
   circle(x,y,diameter);
-  if(x <= 200)
+ if(x <= 250)
         {
             x+=10;
-        } 
-        else if(x > 200 && x <=300)
+        }
+        else if(x == 250 || x <= 300)
         {
-            x+=8;
+            x+=2;
             console.log("second else if for x");
         }
-        else if(x > 300)
+        else if(x != 300)
         {
            x = 50;
         }
-        
+
+      
         if(y <= 200)
         {
-            y+=12;
+            y+=3;
         }
-          
-        else if(y > 200 && y <= 300)
+
+        else if(y == 250 || y <= 300)
         {
-            y+=3; 
+            y+=1; 
             console.log("second else if for y");
         }
-        else if(y > 300)
+        else if(y != 300)
         {
             y = 50;
         }
         if(diameter <= 200)
         {
-            diameter+=10;
+            diameter+=8;
         }
           
-        else if(diameter > 200 && diameter <= 300)
+        else if(diameter == 200 || diameter <= 300)
         {
-            diameter +=4;
+            diameter +=2;
             console.log("second else if for diameter");
         }
-        else if(diameter > 300)
+        else if(diameter != 300)
         {
             diameter = 25;
         }
