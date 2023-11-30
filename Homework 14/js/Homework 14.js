4e3c7651a16d169e1c20754ef82b883f2de5aefd
AWilNol <134312897+AWilNol@users.noreply.github.com>
2023-11-30 14:22:47 -0700

    Update Homework 14.js

--- Homework 14/js/Homework 14.js
+++ Homework 14/js/Homework 14.js
@@ -1,7 +1,4 @@
- var myXs = [];
- var myYs = [];
- var myDiameters = [];
- // x and y for my character
+// x and y for my character
 var characterX = 100;
 var characterY = 100;
 // define the key codes for each letter
@@ -9,6 +6,9 @@ var w = 87;
 var s = 83;
 var a = 65;
 var d = 68;
+// x and y for a shape
+var shapeX = 30;
+var shapeY = 50;
 
 // create a shape when the mouse is clicked
 var mouseShapeX;
@@ -47,18 +47,19 @@ var shape1, shape2;
     characterBoundary();
     // create the shape based on the mouse click
     mouseClack();
+    getRandomNumber();
 }
 
 function saySo()
 {
   textSize(18);
-  text("Next-ZOMBIES! >", width - 170, height - 50)
+  text("Next-ZOMBIES! >",width-170,height-50)
 }
 
 function mouseClack()
 {
   fill(55,130,201);
-  circle(mouseShapeX, mouseShapeY, 30);  
+  circle(mouseShapeX,mouseShapeY,30);  
 }
 
 function createEnemy()
@@ -130,7 +131,6 @@ function createCharacter(x,y)
 {
   characterX = x;
   characterY = y;
-  console.log(characterX);
 }
 
 function drawCharacter()
@@ -146,7 +146,7 @@ function createBorders(thickness)
   // left border
   rect(0,0,thickness,height);
   // bottom border
-  rect(0, height-thickness,width, thickness);
+  rect(0,height-thickness,width,thickness);
   // right upper border
   rect(width-thickness,0,thickness,height-50);
 }
@@ -158,13 +158,18 @@ if(characterX > width && characterY > width - 50)
       fill(0);
       stroke(5);
       textSize(26);
-      text("Congrats!", width / 2 - 50, height / 2 - 50);
+      text("Congrats!",width/2-50,height/2-50);
     }   
 }
 function mouseClicked()
 {
   mouseShapeX = mouseX;
   mouseShapeY = mouseY;
+}
+
+function getRandomNumber(number)
+{
+    return Math.floor(Math.random() * number) + 10;
 }
 
 function keyPressed() {
